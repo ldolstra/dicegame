cca19a140881d97916399a52f95e1ed017d8bf1b
ldolstra <51115395+ldolstra@users.noreply.github.com>
2019-07-13 18:08:02 +0200

    Add files via upload
    My dice game

---
+++ index.js
@@ -0,0 +1,16 @@
+function roll() {
+
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "<span style='color:red'>🚩</span> Player 1 Wins";
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins <span style='color:red'>🚩</span>";
+  } else {
+    document.querySelector("h1").innerHTML = "Draw";
+  }
+}
